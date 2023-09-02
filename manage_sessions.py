2d67c938b04e6061c897ff27b3de2e3ffe33421b
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2023-09-02 21:24:16 +0530

    Pushing new items

---
+++ manage_sessions.py
@@ -0,0 +1,8 @@
+# Interview questions with GreenArc Capital Pte Ltd -
+
+# 1. Write a function/program to create and hold a session.
+# 2. Write a function/program to implement login functionality in Python.
+# 3. Write a function/program to search for Anagrams from a list items.
+
+# solution approach followed - https://ytube.io/3jDy
+
