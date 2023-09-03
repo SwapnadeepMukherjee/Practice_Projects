f3bf7114d9a28d458dfbe8e6793a6f5264c4b050
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2023-09-03 08:53:30 +0530

    Pushing new items

--- manage_sessions.py
+++ manage_sessions.py
@@ -5,4 +5,22 @@
 # 3. Write a function/program to search for Anagrams from a list items.
 
 # solution approach followed - https://ytube.io/3jDy
+from collections import defaultdict
+# from collections.abc import dict_values
+from typing import Any
 
+
+class Solution:
+    def groupAnagrams(self, strs: list[str]):
+        # res = {} # mapping char-count to list of anagrams
+        res = defaultdict(list)  # mapping char-count to list of anagrams
+
+        for s in strs:
+            char_count = [0] * 26  # a..z
+
+            for c in s:
+                char_count[ord(c) - ord("a")] += 1
+
+            res[tuple(char_count)].append(s)
+
+        return res.values()
