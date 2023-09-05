61e41b6df02079343c5dd84fa2fa1ab683e2d34d
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2023-09-05 12:08:03 +0530

    Pushing new items

---
+++ Interactive Brokers/interview.py
@@ -0,0 +1,17 @@
+# Questions:
+
+# 1. Introduce yourself.
+# 2. What skills do you possess along with Knowledge and Competencies.
+# 3. Write a SQL query to join two given tables and return the resulting outcome.
+# 4. Write a pandas merge query to perform the same operation as asked in Question 2.
+# 5. What is inheritance? Different Types of Inheritance in Python(three) with example.
+# 6. What is the difference between class method, static method, instance method?
+# 7. Have you ever worked in Unix system? If so, what is the use of Grep command? Why do we use Grep?
+# 8. What how and why do we use cat command?
+# 9. How do you modify files in unix environment?
+# 10. What are the different data-types that you have used?
+# 11. What is generator function?
+# 12. How can we divide a chunk of 1 million data into chunks of 10,000 and pass it in a function?
+# 13.
+# 14.
+# 15.
