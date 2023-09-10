8ff3dc1facdebc20eeb743ee30d40d95d98c8d7e
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2023-09-10 22:06:06 +0530

    Pushing new items

--- Cognizant/interview-1.py
+++ Cognizant/interview-1.py
@@ -1,4 +1,4 @@
-# Problem statement - https://www.geeksforgeeks.org/python-extract-numbers-from-list-of-strings/
+# 1. Question - https://www.geeksforgeeks.org/python-extract-numbers-from-list-of-strings/
 
 # Solution Approach -
 
@@ -6,40 +6,40 @@
 
 import re
 
-input = "I was 27 years 4 years ago"
+desired_input = "I was 27 years 4 years ago"
 # output = [27, 4]
 
-output = []
-
-output = re.findall('\d+', input)
+output = re.findall('\d+', desired_input)
 print(output)
 
-# 2. Second -
+# # 2. Second -
+#
+# import re
+#
+#
+# def extract_numbers(string):
+#     numbers = re.findall(r'\d+', string)
+#     return list(numbers)
+#
+#
+# input_string = "I was 27 years 4 years ago"
+# numbers_list = extract_numbers(input_string)
+# print(numbers_list)
 
-import re
+# 2. Question - Given an array find the middlemost-element.
 
-
-def extract_numbers(string):
-    numbers = re.findall(r'\d+', string)
-    return list(numbers)
-
-
-input_string = "I was 27 years 4 years ago"
-numbers_list = extract_numbers(input_string)
-print(numbers_list)
-
-# 3. Third -
+import math
 
 in_array = []
 inp = int(input("Please enter your array length"))
 
 for i in range(inp):
-    in_arr = int(input("Please enter element of the array"))
+    in_arr = input("Please enter element of the array")
     in_array.append(in_arr)
 
 sort_in_arr = sorted(in_array)
 
 if inp % 2 == 0:
-    print(sort_in_arr[int(inp / 2)], sort_in_arr[int(inp / 2 + 1)])
+    print(math.floor(int(sort_in_arr[int(inp / 2)])), math.floor(int(sort_in_arr[int(inp / 2 + 1)])))
 else:
-    print(inp / 2)
+    print(math.floor(int(sort_in_arr[int(inp / 2)])))
