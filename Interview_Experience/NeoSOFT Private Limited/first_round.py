b197579154c26a01a5e1577ae03288116428b741
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2023-09-27 11:23:08 +0530

    Pushing new items

---
+++ Interview_Experience/NeoSOFT Private Limited/first_round.py
@@ -0,0 +1,37 @@
+# Questions -
+
+# 1. Introduce yourself.
+
+# 2. What skills do you possess along with Knowledge and Competencies.
+
+# 3. What is the Difference Between a Shallow Copy and Deep Copy?
+
+# 4. What is the difference between list and tuples in Python? which is more memory efficient and why?
+
+# 5. What are decorators in Python?
+
+# 6. What do you mean by Polymorphism in OOPs? What is the difference between method overloading and method overriding?
+
+# 7. How do you merge two dictionaries in Python?
+
+# 8. Discuss Django Architecture and explain how a Django project looks like.
+
+# 9.
+
+# 10. What are the drawbacks of ORM model?
+
+# 11. What is CORS?
+
+# 12. What are SQL Joins?
+
+# 13.
+
+# 14.
+
+# 15.
+
+# 16.
+
+# 17.
+
+# 18.
