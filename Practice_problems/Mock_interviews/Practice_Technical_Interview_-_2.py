34fb656e76869d85807c56ca95b0e8d70e7c216f
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2023-10-28 23:56:28 +0530

    Delete Practice_problems/Mock_interviews/Practice_Technical_Interview_-_2.py

--- Practice_problems/Mock_interviews/Practice_Technical_Interview_-_2.py
+++
@@ -1,9 +0,0 @@
-# 1. What Is The Code to Convert A String to Lower-Case In Python?
-# 2. How to Remove Duplicate Elements From A List?
-# 3. How to Remove Spaces From A String?
-# 4. What Is Pickling And Unpickling?
-# 5. What Is Memory Management In Python?
-# 6. How to Automate Data Extraction From A PDF File And Save It to A CSV Etc.?
-# 7. How to Use Boto3 to Launch An EC2 Instance?
-# 8. How to copy An Object From One S3 Bucket to Another,
-# 9. How to Implement Multi-Factor Authentication For AWS Services And Setting Up IAM Roles?
