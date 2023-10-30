44e57f2965e69b812eea3b3153d1915938512b1f
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2023-10-30 09:06:09 +0530

    Pushing changes
    Signed-off-by: Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>

--- Practice_problems/Mock_interviews/Practice Technical Interview - 1.py
+++ Practice_problems/Mock_interviews/Practice Technical Interview - 1.py
@@ -1,3 +1,75 @@
 # 1. Define / code Bubble sort.
+# Solution -
+
+def bubbleSort(arr):
+    n = len(arr)
+
+    # Traverse through all elements -
+    for i in range(n):
+        swapped = False
+
+        # Last i elements are already in place
+        for j in range(0, n - i - 1):
+
+            # Traverse the array from 0 to n-i-1
+            # Swap if the element found is greater
+            # than the next element
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
+                swapped = True
+        if not swapped:
+            break
+
+
+# Driver code to test above
+if __name__ == "__main__":
+    arr = [64, 34, 25, 12, 22, 11, 90]
+
+    bubbleSort(arr)
+
+    print("Sorted array:")
+    for i in range(len(arr)):
+        print("%d" % arr[i], end=" ")
+
+
 # 2. Define / code Fibonacci Series.
+
+# Fibonacci Series - [0,1,1,2,3,5,8,13]
+
+def fibonacci(n):
+    a = 0
+    b = 1
+    if n < 0:
+        print("Incorrect input")
+    elif n == 0:
+        return a
+    elif n == 1:
+        return b
+    else:
+        for i in range(2, n + 1):
+            c = a + b
+            a = b
+            b = c
+        return b
+
+
 # 3. Define / code Fibonacci Series using recursion.
+
+# Python program to display the Fibonacci sequence
+
+def recur_fibo(n):
+    if n <= 1:
+        return n
+    else:
+        return (recur_fibo(n - 1) + recur_fibo(n - 2))
+
+
+nterms = 10
+
+# check if the number of terms is valid
+if nterms <= 0:
+    print("Please enter a positive integer")
+else:
+    print("Fibonacci sequence:")
+    for i in range(nterms):
+        print(recur_fibo(i))
