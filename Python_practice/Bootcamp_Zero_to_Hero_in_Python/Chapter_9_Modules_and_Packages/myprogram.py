77f43223173309b833da5d4f4dfb1e4231fbcc86
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2024-05-27 01:45:28 +0530

    Renaming folder

---
+++ Python_practice/Bootcamp_Zero_to_Hero_in_Python/Chapter_9_Modules_and_Packages/myprogram.py
@@ -0,0 +1,8 @@
+# from mymodule import myfunc
+from MyMainPackage  import main_package
+from MyMainPackage.SubPackage import sub_main_package
+
+main_package.report_main()
+sub_main_package.sub_report()
+
+# myfunc()
