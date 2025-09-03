0a4da420ef7587c169f233dcf9839fc6e650c601
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2025-09-04 01:00:03 +0530

    Pushing  changes

--- Interview_Experience/Mock_interviews/Mock_Interview_Seimens/Mock_IntervIew_2.py
+++ Interview_Experience/Mock_interviews/Mock_Interview_Seimens/Mock_IntervIew_2.py
@@ -13,3 +13,16 @@
 # 4. Have you deployed any AI/Model so far in your experience?
 # 5. Do you also have exprience in CI/CD pipeline?
 # 6. Do you also have exprience in Docker?
+# 7. Did you ever work with Mircoservices/Monitoring microservices?
+# 8. Did every work on code-optimisiation or API-Optimisation for performance?
+# 9. Did you use Rust anytime before?
+# 10. Scenario-1: Team is tasked with building a FastAPI-based backend service. It exposes predictions from a trained model. During testing it was found that the model takes around 6-seconds per request which is not acceptable as per design. The acceptable time per request as per design should be 2-seconds per request. How would you handle this sitatuion to improve latency keeping the API scalable? : First, we would start by findings where the bottleneck lies using tools. At a high-level, we can start by batching the request. We could further introduce end-points in fast-API to increase concurrency. 
+# 11. Scenario-2: Reviewing developer's code and noticed has that the developer has placed all the database queries and business logic in a single-file. Whenever we are trying to test the service, it's hard to test and hard to extend. You are expected give feedback to the developer. 
+# 12. Design a REST-API based service / product (SaaS), tell your approach.
+
+# Feedback and Suggestions:
+
+# 1. Underprepared for the conversation.
+# 2. AI/ML related concepts were a key part of the conversation.
+# 3. Approach, Commns is good but needs to be a more crisp, to the point and use techninal jargons.
+# 4. Prepare scenario's based on the discussion.
