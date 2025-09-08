b2d283618812b6a0d1ea1c90fd992c8593ccae99
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2025-09-09 01:01:16 +0530

    Pushing changes

--- Interview_Experience/Mock_interviews/Mock_Interview_Seimens/Mock_IntervIew_2.py
+++ Interview_Experience/Mock_interviews/Mock_Interview_Seimens/Mock_IntervIew_2.py
@@ -8,9 +8,9 @@
 # In short, make your answer crisp, more meaningful, take examples to explain, quote your experience.
 
 # 1. Introduce yourself with relevant experience in accordance with your JD.
-# 2. Eplain OOPs concepts any three for example Encapsulation, Abstraction, Inheritance with quoting where you implemented in your BAU project?
+# 2. Explain OOPs concepts any three for example Encapsulation, Abstraction, Inheritance with quoting where you implemented in your BAU project?
 # 3. Are you familar with design patterns? If so explain which design patterns you have used with quoting where you implemented in your BAU project?
-# 4. Have you deployed any AI/Model so far in your experience?
+# 4. Have you deployed any AI/Model so far in your experience? 
 # 5. Do you also have exprience in CI/CD pipeline?
 # 6. Do you also have exprience in Docker?
 # 7. Did you ever work with Mircoservices/Monitoring microservices?
