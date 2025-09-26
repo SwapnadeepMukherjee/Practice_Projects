addfcad9f72d3ad94528d383b74a76a6a298c3a7
Swapnadeep Mukherjee <swapnadeep.jobs@gmail.com>
2025-09-26 07:54:38 +0530

    Pushing changes

---
+++ Interview_Experience/Natwest/Interview.py
@@ -0,0 +1,18 @@
+# Following are the questions that were asked by Natwest in their in-person interview:
+
+# 1. Basic informal introcution.
+# 2. What are the challenges that you have faced so far in your experience?
+# 3. Explain and then write the BFS algoriothm.
+# 4. What is a MongoDB?
+# 5. Explain what is an Airflow DAG. Please write algoiorthm in Python3:
+
+# A -> B -> c -> D -> Z
+# |
+# E -> f -> G -> Z
+# |
+# H -> I -> J -> K -> Z
+
+# 6. Write a code to find the output of the following program:
+
+# Input: [1,6,8,9,1]
+# Output: [15, 15, 2]
--- Interview_Experience/Turing/Interview.py
+++ Interview_Experience/Turing/Interview.py
@@ -7,7 +7,7 @@
 
 # 	1. Tell me recently about 1/2 projects of yours and explain in details.
 # 	2. Follow-up question: how did you configure your secrets in your Jenkins server?
-# 	3. Imagine you have huge dataset and we havse setup a Python code that is giving out correct output that is designed with nested loops only. How do you optimize the code? : Pandas, Numpy, List Comprehension, …
+# 	3. Imagine you have huge dataset and we have setup a Python code that is giving out correct output that is designed with nested loops only. How do you optimize the code? : Pandas, Numpy, List Comprehension, …
 # 	4. How will you design a Python application using Docker and write a docker file such that you don't have to manage pipeline manually and the dockerfile itself manages the sync between Production and non-production code? What are the parameters that you will use?
 # 	5. Design a custom exception that will define something and show the output by using it in a function in python.
 
